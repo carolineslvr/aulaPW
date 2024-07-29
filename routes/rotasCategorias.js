fc0dd94525fe428e8645b5534f809d41bb01ba56
Caroline <carol.silveira.98@gmail.com>
2024-07-29 10:00:51 -0300

    Implementação de autenticação e segurança

--- routes/rotasCategorias.js
+++ routes/rotasCategorias.js
@@ -1,15 +1,18 @@
-const {Router, urlencoded} = require('express');
-const { getCategorias, addCategoria, updateCategoria, deleteCategoria, getCategoriaPorCodigo } = require('../controllers/categoriaController');
+const { Router } = require('express');
+const { getCategorias, addCategoria, updateCategoria, 
+    deleteCategoria, getCategoriaPorCodigo } = 
+    require('../controllers/categoriaController');
+const { verificaJWT } = require('../controllers/segurancaController');
 
 const rotasCategorias = new Router();
 
 rotasCategorias.route('/categoria')
-.get(getCategorias)
-.post(addCategoria)
-.put(updateCategoria);
+               .get(verificaJWT, getCategorias)
+               .post(verificaJWT, addCategoria)
+               .put(verificaJWT, updateCategoria);
 
-rotasCategorias.route('/categoria/:codigo')
-.get(getCategoriaPorCodigo)
-.delete(deleteCategoria);
+rotasCategorias.route('/categoria/:codigo') 
+               .get(verificaJWT, getCategoriaPorCodigo)              
+               .delete(verificaJWT, deleteCategoria);
 
-module.exports = {rotasCategorias};
+module.exports = { rotasCategorias };
--- routes/rotasProdutos.js
+++ routes/rotasProdutos.js
@@ -1,16 +1,18 @@
 const { Router } = require('express');
-
-const {  getProdutos, addProduto, updateProduto, deleteProduto, getProdutoPorCodigo } = require('../controllers/produtoController');
+const { getProdutos, addProduto, updateProduto,
+     deleteProduto, getProdutoPorCodigo } = 
+    require('../controllers/produtoController');
+const { verificaJWT } = require('../controllers/segurancaController');
 
 const rotasProdutos = new Router();
 
 rotasProdutos.route('/produto')
-   .get(getProdutos)
-   .post(addProduto)
-   .put(updateProduto)
+               .get(getProdutos)
+               .post(verificaJWT, addProduto)
+               .put(verificaJWT, updateProduto);
 
-rotasProdutos.route('/produto/:codigo')
-   .get(getProdutoPorCodigo)
-   .delete(deleteProduto)
+rotasProdutos.route('/produto/:codigo') 
+               .get(verificaJWT, getProdutoPorCodigo)              
+               .delete(verificaJWT, deleteProduto);
 
 module.exports = { rotasProdutos };
